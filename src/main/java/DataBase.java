3e8a5defc857b92ed1d001448be0928695cebb9b
shark <1998shark@bk.ru>
2018-08-21 23:46:01 +0300

    3

--- src/main/java/DataBase.java
+++ src/main/java/DataBase.java
@@ -1,22 +1,24 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DataBase {
 
     private static Connection connection;
-    private Statement statement;
+    private static Statement statement;
 
 
     public  DataBase(){
         System.out.printf("Object db is created");
     }
 
-//    public static void connect(){
-//        Class.forName("org.sqlite.JDBC");
-//        connection= DriverManager.getConnection("");
-//
-//    }
+    public static void connect() throws ClassNotFoundException, SQLException {
+        Class.forName("org.sqlite.JDBC");
+        connection=DriverManager.getConnection("jdbc:sqlite:main.db");
+        statement=connection.createStatement();
+
+    }
 
 
 }
