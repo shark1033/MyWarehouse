9ae7b1b94931754e453cba65c20b7b74873ed407
Anton <shark10331@gmail.com>
2018-08-21 23:44:00 +0300

    Some changes

--- src/main/java/DataBase.java
+++ src/main/java/DataBase.java
@@ -1,2 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class DataBase {
+
+    private static Connection connection;
+    private Statement statement;
+
+
+    public  DataBase(){
+        System.out.printf("Object db is created");
+    }
+
+//    public static void connect(){
+//        Class.forName("org.sqlite.JDBC");
+//        connection= DriverManager.getConnection("");
+//
+//    }
+
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,17 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+
+
+
+
+
+
+
+
 }
